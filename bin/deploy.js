a75239bd5fc24b3306d3b4993874c9bf97884668
lqf <1454554357@qq.com>
2022-07-23 18:27:05 +0800

    feat: 支持远程下载和模板下载

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,9 +5,11 @@ const initMethod = require("../lib/init");
 program.version(PackJson.version);
 program
   .command("init <name>")
-  .option("-t, --template <template>", "选择一个模板类型", "vue-ts")
-  .action((name, template) => {
-    initMethod(name, template);
+  .option("-t, --template <template>", "模板类型", "vue-ts")
+  .option("-r, --remote <remote>", "要克隆远程git地址")
+  .action((name, options) => {
+    const { template, remote } = options;
+    initMethod({ name, template, remote });
   });
 
 program.parse(process.argv);
